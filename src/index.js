315993486e9cac0bbdeca905b4aa6f18c1754f8d
dongmeiqi <1125479840@qq.com>
2021-03-02 13:57:16 +0800

    lesson1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import src1 from './assets/1.jpg';
+import src2 from './assets/2.jpg';
+import src3 from './assets/3.jpg';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./index.css";
+const srcs = [src1, src2, src3];
+let index = 0;
+let timer;
+const container =  document.getElementById('root');
+function render () {
+    ReactDOM.render(<img src={srcs[index]} alt={index}></img>, container)
+}
+function start () {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        index = (index + 1) % 3;
+        render()
+    }, 1000)
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+container.onmouseenter = () => {
+    clearInterval(timer);
+}
+container.onmouseleave = () => {
+    start()
+}
+render()
+start()
